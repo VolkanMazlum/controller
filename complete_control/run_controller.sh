8a88498a9316839dd7e8f4fc9df201270f95c75a
benedettagambosi <benedetta.gambosi@mail.polimi.it>
2022-03-01 15:33:57 +0100

    changed number of MPI

--- complete_control/run_controller.sh
+++ complete_control/run_controller.sh
@@ -1,5 +1,5 @@
 !#/bin/sh
 cd  /home/benedetta/controller_versions/controller_tuning/complete_control/
-mpirun -n 2 music /home/benedetta/controller_versions/controller_tuning/complete_control/complete.music
+mpirun -n 8 music /home/benedetta/controller_versions/controller_tuning/complete_control/complete.music
 
 cd /home/benedetta/genetic_algorithm
