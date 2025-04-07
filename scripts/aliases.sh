3cb3c66f0e4f4f288022dc2f0e5ac17b9e65eb53
paolo marzolo <pollo.marzo@gmail.com>
2025-04-07 11:29:03 +0200

    included convenience aliases, removed defaults to ensure correct setup

---
+++ scripts/aliases.sh
@@ -0,0 +1,7 @@
+alias ls='ls -h --color=auto'
+alias l='ls -alFh --color=auto --group-directories-first --time-style=+"%Y-%m-%d %H:%M:%S"'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias envg='env | grep -i' # Usage: envg UID
+alias psg='ps aux | grep -v grep | grep -i' # Usage: psg python
