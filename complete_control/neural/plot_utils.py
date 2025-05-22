73e2446c40736561a4119a501524826705337e9d
paolo marzolo <pollo.marzo@gmail.com>
2025-05-22 19:21:58 +0200

    refactor: cleaned up plot function

--- complete_control/neural/plot_utils.py
+++ complete_control/neural/plot_utils.py
@@ -5,7 +5,7 @@ import structlog
 from mpi4py import MPI
 
 from .Controller import Controller
-from .population_view import plotPopulation
+from .population_view import plot_population
 
 _log: structlog.stdlib.BoundLogger = structlog.get_logger(__name__)
 
@@ -25,76 +25,56 @@ def plot_controller_outputs(
     njt = len(controllers)
     lgd = [f"DoF {i}" for i in range(njt)]
 
-    _log.debug("Generating plots...")
+    _log.debug("Generating plots...", time_len=len(total_time_vect_concat))
 
     for i, controller in enumerate(controllers):
         _log.debug(f"Plotting for DoF {i}...")
 
         # Plot Planner
-        fig, ax = plotPopulation(
+        fig, ax = plot_population(
             total_time_vect_concat,
             controller.pops.planner_p,
             controller.pops.planner_n,
-            [],
-            [],
-            [],
-            [],  # Empty lists for reference, time_vecs, legend, styles
             title=f"Planner {lgd[i]}",
             buffer_size=15,
             filepath=path_fig / f"planner_{i}.png",
         )
 
         # Plot Brainstem
-        fig, ax = plotPopulation(
+        fig, ax = plot_population(
             total_time_vect_concat,
             controller.pops.brainstem_p,
             controller.pops.brainstem_n,
-            [],
-            [],
-            [],
-            [],  # Empty lists for reference, time_vecs, legend, styles
             title=f"Brainstem {lgd[i]}",
             buffer_size=15,
             filepath=path_fig / f"brainstem_{i}.png",
         )
 
         # Plot MC Output
-        fig, ax = plotPopulation(
+        fig, ax = plot_population(
             total_time_vect_concat,
             controller.pops.mc_out_p,
             controller.pops.mc_out_n,
-            [],
-            [],
-            [],
-            [],  # Empty lists for reference, time_vecs, legend, styles
             title=f"MC Out {lgd[i]}",
             buffer_size=15,
             filepath=path_fig / f"mc_out_{i}.png",
         )
 
         # Plot State Estimator
-        fig, ax = plotPopulation(
+        fig, ax = plot_population(
             total_time_vect_concat,
             controller.pops.state_p,
             controller.pops.state_n,
-            [],
-            [],
-            [],
-            [],  # Empty lists for reference, time_vecs, legend, styles
             title=f"State Out {lgd[i]}",
             buffer_size=15,
             filepath=path_fig / f"state_{i}.png",
         )
 
         # Plot Sensory Neurons
-        fig, ax = plotPopulation(
+        fig, ax = plot_population(
             total_time_vect_concat,
             controller.pops.sn_p,
             controller.pops.sn_n,
-            [],
-            [],
-            [],
-            [],  # Empty lists for reference, time_vecs, legend, styles
             title=f"Sensory Out {lgd[i]}",
             buffer_size=15,
             filepath=path_fig / f"sensoryneuron_{i}.png",
